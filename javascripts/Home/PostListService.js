3753660a80348ff64050549b43ebb1f9dcd626c4
Andre Geng <826962871@qq.com>
2015-07-13 11:54:45 +0800

    bug fix: relative url 404 not found error

--- javascripts/Home/PostListService.js
+++ javascripts/Home/PostListService.js
@@ -2,14 +2,15 @@ angular.module('MyBlog')
   .factory('PostList', ['$q', '$http', '$stateParams', function ($q, $http, $stateParams) {
     var convertFormat = function(data){
       data.postContent = data.postContent.replace('.md', '.html');
-      data.postContent = '../digested_posts/' + data.postContent;
+      data.postContent = '/digested_posts/' + data.postContent;
       return data;
     };
+    var serviceUrl = '/data/PostList.json';
     return {
       fetch: function(){
         var deferred = $q.defer();
 
-        $http.get('./data/PostList.json').success(function(data, status, headers, config){
+        $http.get(serviceUrl).success(function(data, status, headers, config){
           deferred.resolve(data);
         }).error(function(data, status, headers, config){
           deferred.reject(data);
@@ -20,7 +21,7 @@ angular.module('MyBlog')
       fetchWithLimit: function(limitNum){
         var deferred = $q.defer();
 
-        $http.get('./data/PostList.json').success(function(data, status, headers, config){
+        $http.get(serviceUrl).success(function(data, status, headers, config){
           deferred.resolve(data.sort(function(item, otherItem){
             if(item.date > otherItem.date){
               return -1;
@@ -38,7 +39,7 @@ angular.module('MyBlog')
       getEssentialPostList: function(){
         var deferred = $q.defer();
 
-        $http.get('./data/PostList.json').success(function(data, status, headers, config){
+        $http.get(serviceUrl).success(function(data, status, headers, config){
           var essentialPostList = data.filter(function(item){
             return item.essential;
           });
@@ -51,7 +52,7 @@ angular.module('MyBlog')
 
       get: function(){
         var deferred = $q.defer();
-        $http.get('./data/PostList.json').success(function(data, status, headers, config){
+        $http.get(serviceUrl).success(function(data, status, headers, config){
           for(var i=0;i<data.length;i++){
             var entry = data[i];
             if(entry.id == $stateParams.id){
