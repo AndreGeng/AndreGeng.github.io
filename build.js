4f311f68fb3333833cb81852231308b94227d7bc
GengAndre <gengze@meituan.com>
2017-03-02 16:31:38 +0800

    add jsdoc comments

--- build.js
+++ build.js
@@ -3,37 +3,12 @@ let fs = require('fs');
 let open = require('open');
 let chalk = require('chalk');
 
-console.log(chalk.green('build started!'));
-let posts = fs.readdirSync(path.resolve(__dirname, 'posts'));
-let postList = posts.map((item) => {
-  let fileContent = fs.readFileSync(path.resolve(__dirname, 'posts', item)).toString('utf8');
-  return {
-    date: getCreateDate(item),
-    subject: getSubject(item, fileContent),
-    summary: getSummary(item, fileContent),
-    postContent: item
-  };
-})
-.sort((itemOne, itemTwo) => {
-  if (itemOne.date > itemTwo.date) {
-    return 1;
-  } else if (itemOne.date < itemTwo.date) {
-    return -1
-  } else {
-    return 0;
-  }
-})
-.map((item, index, list) => {
-  item.id = index + 1;
-  return item;
-})
-.reverse();
-fs.writeFileSync(path.resolve(__dirname, 'data', 'PostList.json'), JSON.stringify(postList), {encoding: 'utf8'});
-console.log(chalk.green('build finished!'));
+generatePostList();
 
 console.log(chalk.green('buildposts started!'));
 let exec = require('child_process').exec;
 new Promise((resolve, reject) => {
+  // transform markdown to html
   exec('gulp buildposts', (err, stdout, stderr) => {
     if (err) reject(err);
     console.log(chalk.green('buildposts finished!'));
@@ -41,12 +16,14 @@ new Promise((resolve, reject) => {
   });
 })
 .then(() => {
+  // start server
   exec('http-server', (err, stdout, stderr) => {
     if(err) Promise.reject(err);
     console.log(chalk.green('http-server started!'));
   });
 })
 .then(() => {
+  // open blog url for regression test
   open('http://127.0.0.1:8080');
 })
 .catch((e) => {
@@ -54,7 +31,43 @@ new Promise((resolve, reject) => {
   console.log(chalk.red('Error:' + e));
 });
 
-
+/**
+ * generate data/PostList.json(used to show postlist page)
+ */
+function generatePostList() {
+  console.log(chalk.green('build started!'));
+  let posts = fs.readdirSync(path.resolve(__dirname, 'posts'));
+  let postList = posts.map((item) => {
+    let fileContent = fs.readFileSync(path.resolve(__dirname, 'posts', item)).toString('utf8');
+    return {
+      date: getCreateDate(item),
+      subject: getSubject(item, fileContent),
+      summary: getSummary(item, fileContent),
+      postContent: item
+    };
+  })
+  .sort((itemOne, itemTwo) => {
+    if (itemOne.date > itemTwo.date) {
+      return 1;
+    } else if (itemOne.date < itemTwo.date) {
+      return -1
+    } else {
+      return 0;
+    }
+  })
+  .map((item, index, list) => {
+    item.id = index + 1;
+    return item;
+  })
+  .reverse();
+  fs.writeFileSync(path.resolve(__dirname, 'data', 'PostList.json'), JSON.stringify(postList), {encoding: 'utf8'});
+  console.log(chalk.green('build finished!'));
+}
+/**
+ * get file created date from filename
+ * @param  {string} filename
+ * @return {string} file created date
+ */
 function getCreateDate(filename) {
   let regex = /^[a-zA-Z0-9&]*_([0-9]{4}-[0-9]{2}-[0-9]{2})\.md$/;
   let matchResult = regex.exec(filename);
@@ -65,6 +78,12 @@ function getCreateDate(filename) {
   }
 }
 
+/**
+ * get post subject
+ * @param  {string} filename
+ * @param  {string} fileContent
+ * @return {string} post's subject
+ */
 function getSubject(filename, fileContent) {
   let regex = /##[\s]*([^\n]+)/;
   let matchResult = regex.exec(fileContent);
@@ -75,6 +94,12 @@ function getSubject(filename, fileContent) {
   }
 }
 
+/**
+ * get post summary
+ * @param  {string} filename
+ * @param  {string} fileContent
+ * @return {string} file summary
+ */
 function getSummary(filename, fileContent) {
   let regex = /###[\s]*([^\n]+)/;
   let matchResult = regex.exec(fileContent);
